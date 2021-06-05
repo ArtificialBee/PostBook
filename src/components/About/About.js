7abbff7e73b2ddc84a1587b3dd6bf88dfe2151f5
ArtificialBee <edvinsacic@outlook.com>
2021-06-05 17:36:02 +0200

    Added 'about' section

---
+++ src/components/About/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './About.css'
+
+const About = (props) => {
+    return (
+        <div className="aboutCard">
+            <h2 className="about">About</h2>
+            <div className="info">
+                <p>Address: <span>{props.s}</span></p>
+                <p>Birthday: <span>{props.bday.join(" ")}</span></p>
+                <p>Elementary school: <span>{props.es}</span></p>
+                <p>High school: <span>{props.hs}</span></p>
+                <p>College: <span>{props.co}</span></p>
+                <p>Hobbies: <span>{props.h}</span></p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './App.css';
 /*Import components*/
 import Header from '../components/header/Header'
-import About from '../components/About'
+import About from '../components/About/About'
 import News from '../components/News'
 import Content from '../components/content/Content'
 import img from '../images/ja_rounded.png'
@@ -14,10 +14,12 @@ class App extends Component {
       name: "Edvin Sačić"
     },
     about: {
-      birh_day: ["29", "Oct", "1997"],
-      elem_school: "O.S. Mirsad Prnjavorac",
-      high_school: "Srednja elemtrotehnicka skola Sarajevo",
-      collage: "Elektrotehnicki fakultet Sarajevo"
+      street: "Igmanska do br. 75, Vogošća",
+      birth_day: ["29", "Oct", "1997"],
+      elem_school: "O.Š. Mirsad Prnjavorac",
+      high_school: "Srednja elektrotehnička škola Sarajevo",
+      collage: "Elektrotehnički fakultet Sarajevo",
+      hobbies: "Make webpages"
     },
     news: {
       notifs: "No notification",
@@ -27,12 +29,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header img={this.state.main.picture} name={this.state.main.name} />
-        {/* <div>
-          <About />
-          <Content />
+        <Header
+          img={this.state.main.picture}
+          name={this.state.main.name} />
+        <div>
+          <About
+            s={this.state.about.street}
+            bday={this.state.about.birth_day}
+            es={this.state.about.elem_school}
+            hs={this.state.about.high_school}
+            co={this.state.about.collage}
+            h={this.state.about.hobbies} />
           <News />
-        </div> */}
+        </div>
       </div>
     );
   }
